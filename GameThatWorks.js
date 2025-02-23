e7de589d0b0d586258ed6e72e800e6ce24e3304b
UnownDragon666 <143086245+UnownDragon666@users.noreply.github.com>
2025-02-23 22:39:28 +0000

    Initial commit

---
+++ GameThatWorks.js
@@ -0,0 +1,10 @@
+/*******************************************************/
+// P5 play demo game
+// Create a demo game
+// Written by Idrees Munshi
+/*******************************************************/
+
+function setup() {
+    cnv = new Canvas(windowWidth, windowHeight - 100);
+    
+}
